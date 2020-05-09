d6fdabad4c26deaa98a947222bf6bd816cc1c5c9
MatthiasPELLACOEUR <matthias.pellacoeur@hotmail.com>
2020-05-09 22:03:03 +0200

    start operations in js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,96 @@
 let buttons = document.getElementsByClassName('button')
 let result = document.getElementById('result')
 let acButton = document.getElementById('AC')
+let btnOperations = document.getElementsByClassName('button-operations')
+let inputButton = document.getElementsByTagName('button')
+let operation = ""
 
 
-// buttons.addEventListener('click', function(e){
-//     console.log(buttons.value);
+function getSum(x, y) {
+    var r = x + y
+    console.log(r);
     
-// })
+}
+
 
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function(e){
         // console.log(e.target.textContent);
         let nbTwo = document.createTextNode(e.target.textContent)
-        if(result.textContent == '0'){
-            result.textContent = e.target.textContent 
+        
+
+        if(result.textContent == '0' && e.target.textContent >= '1'){
+            result.textContent = e.target.textContent
             acButton.textContent = "C"
         }
         else if(result.textContent !== '0') {
             result.appendChild(nbTwo)
         }
+    })
+}
+for (var i = 0; i < inputButton.length; i++) {
+    inputButton[i].addEventListener('click',function(e){
+        // console.log(e.target.classList[0]);
+
+        
+        switch (e.target.classList[0]) {
+            case 'numbers': 
+            break;
+            case 'button-operations':
+                console.log(e.target.textContent);
+                
+                switch (e.target.textContent){
+                    case '/':
+                        var x = parseInt(result.textContent, 10)
+                        operation = '/'
+                        
+                    break
+
+                    case '*':
+                        var x = parseInt(result.textContent, 10)
+                        operation = '*'
+
+                    break
+
+                    case '+':
+                        var x = parseInt(result.textContent, 10)
+                        operation = '+'
+
+                    break
+
+                    case '-':
+                        var x = parseInt(result.textContent, 10)
+                        operation = '-'
+
+                    break
+
+                    case '=':
+                        var y = parseInt(result.textContent, 10)
+                        switch (operation){
+                            case '+':
+                                getSum(x, y) 
+                            break
+                        }
+                        
+                    break
+                }         
+                
+                if(e.target.textContent == '+'){
+                    var x = parseInt(result.textContent, 10)
+                    console.log(x)
+                    result.textContent = "0"
+                }else if(e.target.textContent == '='){
+                    var y = parseInt(result.textContent, 10)
+                    console.log(y);
+                }
+                getSum();
+                
+               
+                break;
+            default:
+                console.log("t'es nul");
+        }
+
     })
 }
 
