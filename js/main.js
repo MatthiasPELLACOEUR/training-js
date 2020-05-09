61800e7f83d681bd1dab3df7eaf7142ec62d3bcb
MatthiasPELLACOEUR <matthias.pellacoeur@hotmail.com>
2020-05-09 06:49:32 +0200

    started js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let buttons = document.getElementsByClassName('button')
+let result = document.getElementById('result')
+let acButton = document.getElementById('AC')
+
+
+// buttons.addEventListener('click', function(e){
+//     console.log(buttons.value);
+    
+// })
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function(e){
+        // console.log(e.target.textContent);
+        let nbTwo = document.createTextNode(e.target.textContent)
+        if(result.textContent == '0'){
+            result.textContent = e.target.textContent 
+            acButton.textContent = "C"
+        }
+        else if(result.textContent !== '0') {
+            result.appendChild(nbTwo)
+        }
+    })
+}
